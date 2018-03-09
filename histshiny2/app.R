c0625e3488647e4ddeba99a898647fd4b031db51
gecko <beth.g.johnson@gmail.com>
2018-03-09 00:21:33 -0500

    added histogram shiny plot

--- histshiny2/app.R
+++ histshiny2/app.R
@@ -1,27 +1,52 @@
 library(shiny)
+library(ggplot2)
+prop.sale<-readRDS("data/propsale.rds")
+
+
+
+my.col<-c("PRICE","darkgreen")
 
 # Define UI for app that draws a histogram ----
 ui <- fluidPage(
   
-  # App title ----
+  # App title >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
   titlePanel("Hello Shiny!"),
   
   # Sidebar layout with input and output definitions ----
+  #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
   sidebarLayout(
     
     # Sidebar panel for inputs ----
     sidebarPanel(
-      
+    #----------------------------------------------------------------------------------
+      selectInput('prop.col', 'Property Characteristic', c('None', names(prop.sale))),
+    
       # Input: Slider for the number of bins ----
       sliderInput(inputId = "bins",
                   label = "Number of bins:",
                   min = 1,
                   max = 50,
-                  value = 30)
+                  value = 30),
       
-    ),
+      
+      
+      sliderInput(inputId = "min.val",
+                  label = "min value of histogram:",
+                  min = 150000,
+                  max = 500000,
+                  value =100000),
+     
     
-    # Main panel for displaying outputs ----
+      sliderInput(inputId = "max.val",
+                label = "max value of histogram:",
+                min = 150000,
+                max = 500000,
+                value =160000)
+    ),
+    #----------------------------------------------------------------------------
+  #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+    
+    # Main panel for displaying outputs >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
     mainPanel(
       
       # Output: Histogram ----
@@ -30,9 +55,11 @@ ui <- fluidPage(
     )
   )
 )
-
 # Define server logic required to draw a histogram ----
 server <- function(input, output) {
+  
+  
+  prop.sale.sub<-reactive({subset(prop.sale,PRICE>input$min.val & PRICE<input$max.val)})
   
   # Histogram of the Old Faithful Geyser Data ----
   # with requested number of bins
@@ -43,15 +70,9 @@ server <- function(input, output) {
   #    re-executed when inputs (input$bins) change
   # 2. Its output type is a plot
   output$distPlot <- renderPlot({
-    
-    x    <- faithful$waiting
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    
-    hist(x, breaks = bins, col = "#75AADB", border = "white",
-         xlab = "Waiting time to next eruption (in mins)",
-         main = "Histogram of waiting times")
-    
-  })
+    ggplot(prop.sale.sub(),aes_string(my.col[1])) + 
+      geom_histogram(bins=input$bins,fill=my.col[2])+xlim(c(input$min.val,input$max.val))
+    })
   
 }
 
