7bb0f9591080e9475f18a3e81a15b7b83c3f3598
gecko <beth.g.johnson@gmail.com>
2018-03-09 23:25:52 -0500

    shiny working added another function

--- histshiny2/app.R
+++ histshiny2/app.R
@@ -17,7 +17,7 @@ ui <- fluidPage(
     # Sidebar panel for inputs ----
     sidebarPanel(
     #----------------------------------------------------------------------------------
-      selectInput('prop.col', 'Property Characteristic', c('None', names(prop.sale))),
+      selectInput('prop.col', 'Property Characteristic', c("PRICE"=7.0)),
     
       # Input: Slider for the number of bins ----
       sliderInput(inputId = "bins",
@@ -55,16 +55,16 @@ ui <- fluidPage(
 )
 # Define server logic required to draw a histogram ----
 server <- function(input, output) {
-  
+  col.index<-7
   #figure out what the max and min values will be given the slider window
-  min.val.col<-reactive({max(prop.sale[,my.col[1]])*input$min.val})
-  max.val.col<-reactive({max(prop.sale[,my.col[1]])*input$max.val})
+  min.val.col<-reactive({max(prop.sale[,as.integer(input$prop.col)])*input$min.val})
+  max.val.col<-reactive({max(prop.sale[,as.integer(input$prop.col)])*input$max.val})
   #subset the data frame to include only the range of the column we are interested in displaying
   #prop.sale.sub<-reactive({subset(prop.sale,PRICE>=min.val.col() & PRICE<=max.val.col())})
   prop.sale.sub<-reactive({ 
-    prop.sale[prop.sale[,7]>=min.val.col() &  prop.sale[,7]<=max.val.col(), ] 
+    prop.sale[prop.sale[,as.integer(input$prop.col)]>=min.val.col() &  prop.sale[,as.integer(input$prop.col)]<=max.val.col(), ] 
               })
-
+  
   # This expression that generates a histogram is wrapped in a call
   # to renderPlot to indicate that:
   #
@@ -72,9 +72,10 @@ server <- function(input, output) {
   #    re-executed when inputs (input$bins) change
   # 2. Its output type is a plot
   output$distPlot <- renderPlot({
-    ggplot(prop.sale.sub(),aes_string(my.col[1])) + 
+    ggplot(prop.sale.sub(),aes_string("PRICE")) + 
       geom_histogram(bins=input$bins,fill=my.col[2])+
-      xlim(c(min.val.col(),max.val.col()))
+      xlim(c(min.val.col(),max.val.col()))+
+      ggtitle(typeof(input$prop.col))
     })
   
 }
